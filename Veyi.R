0fe6b7f3a1e3162faeca6efd7b411f8bd7a7cce2
CarlosBravoPena <cbravop@yahoo.com>
2020-05-29 22:53:14 -0400

    29 de Mayo 22:53 hrs

--- Veyi.R
+++ Veyi.R
@@ -13,7 +13,7 @@ file<- file.path("C:/Users/Carlos Bravo/Desktop/R/homework-2",filename)
 veyi_wide <- read_excel(file)
 names(veyi_wide)
 head(veyi_wide)
-veyi_tidy_temp <- veyi_wide %>% select("ID","Nombre del Producto","Precio del Producto","Municipio de envío")
+veyi_tidy_temp <- veyi_wide %>% select("ID","Estado de Pago","Nombre del Producto","Precio del Producto","Cantidad de Productos","Municipio de envío")
 
 
 head(veyi_tidy_temp)
@@ -24,14 +24,20 @@ head(new_tidy)
 
 new_tidy$ID <- as.numeric(as.character(new_tidy$ID))
 new_tidy$`Precio del Producto` <- as.numeric(as.character(new_tidy$`Precio del Producto`), string.asFactors=FALSE)
+new_tidy$`Cantidad de Productos` <- as.numeric(as.character(new_tidy$`Cantidad de Productos`), string.asFactors=FALSE)
 head(new_tidy)
+new_tidy<- new_tidy %>% mutate(Total=`Precio del Producto`*`Cantidad de Productos`)
+names(new_tidy)
+head(new_tidy)
+
 
 
 new_tidy
 tickets<- new_tidy %>% group_by(ID) %>%
-  summarise(precio_canasta=sum(`Precio del Producto`),n=n())
+  summarise(precio_canasta=sum(Total),n_productos=n())
 tickets
 mean(tickets$precio_canasta)
+mean(tickets$n)
 grafico_puntos_tickets<-tickets%>% ggplot()+geom_point(aes(x=ID,y=precio_canasta,colour=precio_canasta))+geom_line(aes(x=ID,y=mean(precio_canasta)))
 
 grafico_hist<-tickets%>% ggplot()+geom_histogram(aes(precio_canasta))
@@ -41,7 +47,7 @@ names(new_tidy)
 comuna<- new_tidy %>% filter(!is.na(`Municipio de envío`)) %>% select(ID,`Municipio de envío`)
 
 frec_comuna <- comuna %>% group_by(`Municipio de envío`) %>% count(`Municipio de envío`)
-
+frec_comuna
 grafico_frec_comunas <- frec_comuna %>% ggplot()+geom_point(aes(x=`Municipio de envío`,y=n))
 #grid.arrange(grafico_puntos_tickets,grafico_hist,grafico_frec_comunas,ncol=3)
 grafico_frec_comunas
