81add77af98324048aad2896a478bd391bdd0432
CarlosBravoPena <cbravop@yahoo.com>
2020-05-29 22:22:14 -0400

    29 de mayo 22:22 hrs

--- Veyi.R
+++ Veyi.R
@@ -5,7 +5,7 @@ library(dslabs)
 library(dplyr)
 library(gridExtra)
 
-install.packages("gridExtra")
+
 
 filename <- "Pedidos.xls"
 file<- file.path("C:/Users/Carlos Bravo/Desktop/R/homework-2",filename)
@@ -32,18 +32,20 @@ tickets<- new_tidy %>% group_by(ID) %>%
   summarise(precio_canasta=sum(`Precio del Producto`),n=n())
 tickets
 mean(tickets$precio_canasta)
-grafico_puntos<-tickets%>% ggplot()+geom_point(aes(x=ID,y=precio_canasta,colour=precio_canasta))+geom_line(aes(x=ID,y=mean(precio_canasta)))
+grafico_puntos_tickets<-tickets%>% ggplot()+geom_point(aes(x=ID,y=precio_canasta,colour=precio_canasta))+geom_line(aes(x=ID,y=mean(precio_canasta)))
 
 grafico_hist<-tickets%>% ggplot()+geom_histogram(aes(precio_canasta))
-grid.arrange(grafico_puntos,grafico_hist,ncol=2)
+
 
 names(new_tidy)
 comuna<- new_tidy %>% filter(!is.na(`Municipio de envío`)) %>% select(ID,`Municipio de envío`)
 
 frec_comuna <- comuna %>% group_by(`Municipio de envío`) %>% count(`Municipio de envío`)
 
-frec_comuna %>% ggplot()+geom_point(aes(x=`Municipio de envío`,y=n))
-
-
+grafico_frec_comunas <- frec_comuna %>% ggplot()+geom_point(aes(x=`Municipio de envío`,y=n))
+#grid.arrange(grafico_puntos_tickets,grafico_hist,grafico_frec_comunas,ncol=3)
+grafico_frec_comunas
+grafico_puntos_tickets
+grafico_hist
 
 
