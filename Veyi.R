6961edd26e9ceefca25f8bc071545917c9dd3784
CarlosBravoPena <cbravop@yahoo.com>
2020-06-01 19:12:31 -0400

    1 de junio 19:12 hrs

--- Veyi.R
+++ Veyi.R
@@ -21,14 +21,20 @@ head(veyi_wide)
 #seleccionamos los campos a trabajar desde el archivo que se baja de VEYI.cl
 new_tidy <- veyi_wide %>% select("ID","Estado de Pago","Nombre del Producto","Precio del Producto","Cantidad de Productos","Municipio de envío")
 head(new_tidy)
+library(stringr)
+#alimino el coma cero cero del producto
+new_tidy$`Precio del Producto`<- str_replace_all(veyi_wide$`Precio del Producto`,",00","")
+
+
 #convierto valores caracteres en numericos para campos de precio, ID, cantidad
-new_tidy$ID <- as.numeric(as.character(new_tidy$ID))
-new_tidy$`Precio del Producto` <- as.numeric(as.character(new_tidy$`Precio del Producto`), string.asFactors=FALSE)
-new_tidy$`Cantidad de Productos` <- as.numeric(as.character(new_tidy$`Cantidad de Productos`), string.asFactors=FALSE)
+new_tidy$ID <- as.numeric(new_tidy$ID)
+new_tidy$`Precio del Producto` <- as.numeric(new_tidy$`Precio del Producto`)
+new_tidy$`Cantidad de Productos` <- as.numeric(new_tidy$`Cantidad de Productos`)
 
 #agrego variable TOTAL como producto del precio y la cantidad
 new_tidy<- new_tidy %>% mutate(Total=`Precio del Producto`*`Cantidad de Productos`)
 names(new_tidy)
+head(new_tidy)
 
 #creo tabla Tickets con ID, valor de la canasta, y n como numero de itemes del pedido
 tickets<- new_tidy %>% group_by(ID) %>%
