c20650733b21a1eda062fc85ce0d46263fb0851e
CarlosBravoPena <cbravop@yahoo.com>
2020-06-14 20:44:44 -0400

    final assesment confounding junio 14 20:44 hrs

---
+++ LMAssesment1.R
@@ -0,0 +1,70 @@
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(dslabs)
+#ejercicio de los accesos a financiamiento en holanda
+data("research_funding_rates")
+research_funding_rates
+
+new_table_applications<- research_funding_rates %>%
+  select(applications_men,applications_women,awards_men,awards_women)
+
+names(new_table_applications)
+
+new_table_applications<-new_table_applications%>%
+  mutate(not_aw_men=applications_men-awards_men,not_aw_women=applications_women-awards_women)%>%select(awards_men,awards_women,not_aw_men,not_aw_women)
+
+new_table_applications
+
+app_men<- sum(new_table_applications$applications_men)
+app_women<- sum(new_table_applications$applications_women)
+
+aw_men<- sum(new_table_applications$awards_men)
+aw_women<- sum(new_table_applications$awards_women)
+
+not_aw_men<- app_men-aw_men
+not_aw_women<- app_women-aw_women
+
+app_men
+app_women
+aw_men
+aw_women
+not_aw_men
+not_aw_women
+
+men<- c(aw_men, not_aw_men)
+women<- c(aw_women,not_aw_women)
+table<- data.frame("awarded","not_awarded")
+table["men",]<-men
+table["women",]<- women
+table
+
+#respuesta pregunta 1 
+
+two_by_two <- research_funding_rates %>% 
+  select(-discipline) %>% 
+  summarize_all(funs(sum)) %>%
+  summarize(yes_men = awards_men, 
+            no_men = applications_men - awards_men, 
+            yes_women = awards_women, 
+            no_women = applications_women - awards_women) %>%
+  gather %>%
+  separate(key, c("awarded", "gender")) %>%
+  spread(gender, value)
+two_by_two
+
+#respuesta pregunta 2a
+two_by_two %>% 
+  mutate(men = round(men/sum(men)*100, 1), women = round(women/sum(women)*100, 1)) %>%
+  filter(awarded == "yes") %>%
+  pull(men)
+
+#respuesta pregunta 2b
+two_by_two %>% 
+  mutate(men = round(men/sum(men)*100, 1), women = round(women/sum(women)*100, 1)) %>%
+  filter(awarded == "yes") %>%
+  pull(women)
+
+# test whether gender and admission are independent
+two_by_two %>% 
+  tidy(chisq.test(.))
